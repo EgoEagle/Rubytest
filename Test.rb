f44c684b27ed2e42d41e4bcb4c950394a537c414
Tony <TonyLin3591@gmail.com>
2022-10-01 11:49:49 -0400

    changes

--- Test.rb
+++ Test.rb
@@ -1,2 +1,8 @@
 puts ("Hello"); # starts new line
 print ("Hello"); # same line
+name = "Tony"
+num = 25;
+flag = true;
+
+puts ("Hello" + name);
+puts (num.to_s); #.to_i  .to_f  .to_s   int float string
