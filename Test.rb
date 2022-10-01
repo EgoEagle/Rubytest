c7b03f382f37a8dbcb923edb89f9081ffa75470c
Tony <TonyLin3591@gmail.com>
2022-10-01 12:06:18 -0400

    changes

--- Test.rb
+++ Test.rb
@@ -6,3 +6,12 @@ flag = true;
 
 puts ("Hello" + name);
 puts (num.to_s); #.to_i  .to_f  .to_s   int float string
+puts name.include? "on";
+
+puts("Enter a number");
+num = gets.chomp;
+puts("You entered "+ num);
+
+arraysA = [ "a" , "b" , "c"];
+puts arraysA[0];
+puts arraysA[-1];
