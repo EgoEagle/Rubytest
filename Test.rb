2d023172a04ad29f169952b1e3b247f68bfb1772
Tony <TonyLin3591@gmail.com>
2022-10-02 00:56:23 -0400

    changes

--- Test.rb
+++ Test.rb
@@ -8,10 +8,18 @@ puts ("Hello" + name);
 puts (num.to_s); #.to_i  .to_f  .to_s   int float string
 puts name.include? "on";
 
-puts("Enter a number");
-num = gets.chomp;
-puts("You entered "+ num);
+#puts("Enter a number");
+#num = gets.chomp;
+#puts("You entered "+ num);
 
 arraysA = [ "a" , "b" , "c"];
 puts arraysA[0];
 puts arraysA[-1];
+
+def add(num1=6,num2=2)
+     return num1+num2
+end
+
+puts add(3,4)
+puts add(4,5.5)
+puts add("hello"," world")
